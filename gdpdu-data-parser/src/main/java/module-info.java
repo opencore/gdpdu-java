c29a46c02f7ffc909eb231b67b6965ec882fecc6
Lars Francke <lars.francke@gmail.com>
2020-06-18 00:32:08 +0200

    Fix module by transitively including SLF4J

--- gdpdu-data-parser/src/main/java/module-info.java
+++ gdpdu-data-parser/src/main/java/module-info.java
@@ -4,7 +4,7 @@ module com.opencore.gdpdu.data {
   requires com.opencore.gdpdu.models;
   requires java.desktop;
   requires java.validation;
-  requires org.slf4j;
+  requires transitive org.slf4j;
 
   exports com.opencore.gdpdu.data;
 }
