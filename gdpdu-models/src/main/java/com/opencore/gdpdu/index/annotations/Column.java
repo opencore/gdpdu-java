3bc7c15ceed901d9c9b35eb2a193d5aa1a29ad17
Lars Francke <lars.francke@gmail.com>
2020-06-24 12:43:20 +0200

    Extend the Column annotation to support data types

--- gdpdu-models/src/main/java/com/opencore/gdpdu/index/annotations/Column.java
+++ gdpdu-models/src/main/java/com/opencore/gdpdu/index/annotations/Column.java
@@ -5,10 +5,18 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+import com.opencore.gdpdu.index.models.DataType;
+
 @Target({ElementType.FIELD})
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Column {
 
   String value();
+
+  /**
+   * This is optional and can be used for validation purposes.
+   * This way we can check whether an {@code index.xml} file actually uses the correct GDPdU data types.
+   */
+  DataType type() default DataType.AlphaNumeric;
 
 }
